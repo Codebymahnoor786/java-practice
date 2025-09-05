abbf0a58bce0dd06493c27f0a4b345d1ce1b96ea
Codebymahnoor786 <mughalmnoor78@gmail.com>
2025-09-05 18:58:24 +0500

    Add files via upload

---
+++ keywords and identifiers.java
@@ -0,0 +1,9 @@
+// Example of Keywords and Identifiers
+public class Test {   // 'public' and 'class' are keywords, 'Test' is identifier
+    public static void main(String[] args) {  // 'static', 'void' are keywords, 'main' is identifier
+        int age = 20;   // 'int' is keyword, 'age' is identifier
+        String name = "Ali"; // 'String' is class (not keyword), 'name' is identifier
+
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
