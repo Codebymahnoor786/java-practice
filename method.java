35979b73cb6d335f51544e4e7e80d62a39caff2f
Codebymahnoor786 <mughalmnoor78@gmail.com>
2025-09-05 18:27:02 +0500

    Add files via upload

---
+++ method.java
@@ -0,0 +1,11 @@
+
+public class variables {
+    public static int Add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int sum = Add(100, 100);
+        System.out.println("The sum is: " + sum);
+    }
+}
