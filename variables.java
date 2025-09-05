f64b1ff4e871afd174a7e50a45d5a968654dfa9c
Codebymahnoor786 <mughalmnoor78@gmail.com>
2025-09-05 17:23:40 +0500

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,10 @@
+public class variables{
+
+
+             public static void main(String[] args){
+                int num1 = 7;
+                int num2 = 21;
+                int sum = num1+num2;
+             System.out.println("the sum is:"+ sum);
+             }
+}
