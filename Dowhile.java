65d260f9e47408404a67a319ba4bfa1461e2d725
Codebymahnoor786 <mughalmnoor78@gmail.com>
2025-09-12 10:56:15 +0500

    Add files via upload

---
+++ Dowhile.java
@@ -0,0 +1,11 @@
+public class DoWhileExample {
+    public static void main(String[] args) {
+        int i = 1;
+
+        // do-while loop
+        do {
+            System.out.println("Number: " + i);
+            i++;
+        } while (i <= 5); // condition checked after execution
+    }
+}
