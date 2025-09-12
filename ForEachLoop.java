b9f0d1b0edb9f4b354f8d40d71b34b22f5e3eeaa
Codebymahnoor786 <mughalmnoor78@gmail.com>
2025-09-12 11:01:26 +0500

    Add files via upload

---
+++ ForEachLoop.java
@@ -0,0 +1,11 @@
+public class ForEachExample {
+    public static void main(String[] args) {
+        // array of numbers
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // for-each loop
+        for (int num : numbers) {
+            System.out.println("Value: " + num);
+        }
+    }
+}
